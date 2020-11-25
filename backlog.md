1fee8547c3b0b5d735e2debba7b93eb722e15119
Rahul Agrawal <ragrawal@mphrx.com>
2020-11-25 09:57:04 +0530

    First test

---
+++ backlog.md
@@ -0,0 +1,8 @@
+
+Requirements:
+
+0. Run a local timer with sound
+1. Join a timer group
+2. Initiate a timer for x mins - default to 5 mins
+3. To be notified when a timer goes off for anyone
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+fn timer(duration: u64, callback: &dyn Fn(&mut String),  status: &mut String ) {
+    // sleep(duration);
+    callback(status);
+}
+
+
+fn done(message: &mut String) {
+    *message = "New String".to_string();
+    println!("I'm done");
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn calls_back_on_timer_completion() {
+        let mut status: String = String::new();
+        timer(5, &done, &mut status);
+
+        // let dir_option = readdir("assets");
+        // assert_eq!(dir_option.is_ok(), true);
+    }
 }
