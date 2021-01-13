27bc30f18c915841ecc3e0e9446e21df29ac96e0
Simranveer Grewal <sgrewal@mphrx.com>
2021-01-12 23:23:26 -0500

    added function for calculating endtime

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use async_std::task;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::{Arc, RwLock};
 use std::time::Duration;
+use chrono::{DateTime, Utc, Local};
 extern crate firebase_rs;
 use firebase_rs::*;
 
@@ -29,6 +30,21 @@ async fn timer(
 
 fn done(ran: Arc<AtomicBool>) {
     ran.store(true, Ordering::SeqCst);
+}
+
+fn store_future_time(givenTime: Option<i64>, how_long: i64) {
+    let start_time = match givenTime {
+        Some(time) => time,
+        None => Utc::now().timestamp(),
+    };
+
+    println!("startTime: {:?}", start_time);
+    
+    let end_time = start_time + how_long * 60 * 1000;
+    //let utc_time = DateTime::<Utc>::from_utc(local_time.naive_utc(), Utc);
+
+    println!("end time: {}", end_time);
+
 }
 
 #[cfg(test)]
@@ -59,4 +75,16 @@ mod tests {
         assert_eq!(res.body, "{\"username\":\"test\"}");
 
     }
+
+    #[test]
+    fn save_future_time_from_duration() {
+        let duration: u32 = 5;
+
+        store_future_time(None, 5);
+
+        // assert_eq!(... a string holding the UTC 5 minutes out..., if weask for what was sent to Firebase)
+//        println!("{:?}", utc_time);
+    }
+
+    
 }
