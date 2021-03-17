9f4c174e7b2d48198c8c056150d3989e43b10ba7
Jeff L <jeff@langrsoft.com>
2021-03-16 21:22:33 -0600

    externalize retrieve of firebase

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
 use async_std::task;
+use chrono::Utc;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use std::time::Duration;
-use chrono::Utc;
 extern crate firebase_rs;
 use firebase_rs::*;
-extern crate machine_uid;
 extern crate clap;
-use clap::{Arg, App, SubCommand};
-use serde_json::{Value, Deserializer};
+extern crate machine_uid;
 use anyhow::Result;
-
-//static FIREBASE_URL: &str = "https://rust-timer-default-rtdb.firebaseio.com";
-static FIREBASE_URL: &str = "rust-timer-default-rtdb.firebaseio.com";
-
+use clap::{App, Arg, SubCommand};
+use serde_json::{Deserializer, Value};
 
 fn main() {
     // let matches = App::new("My Super Program")
@@ -21,8 +17,11 @@ fn main() {
     //      .subcommand(SubCommand::with_name("join").arg(Arg::with_name("id").required(true)))
     //      .get_matches();
     // if let Some(matches) = matches.subcommand_matches("new") {
+    // TODO: extract this chunk of "sequencing" code to a function
+    //       ... and write some tests against *it*
     //     let id: String = machine_uid::get().unwrap();
-    //     let end_time = store_future_time(None, 1, id.as_str());
+    //     let firebase = firebase();
+    //     let end_time = store_future_time(firebase, None, 1, id.as_str());
     //     println!("Timer start, id: {}", id);
     //     // TODO: remove unwrap?
     //     task::block_on(task::spawn(notify_at(end_time.unwrap(), notification, Arc::new(AtomicBool::new(false)))));
@@ -43,21 +42,20 @@ fn main() {
     //     // 2: (timer expired)
     //     // 3: (invalid id)
     //     println!("id: {}", id);
-    // }
+
+
+    // let firebase =
+    //     firebase().map_err(|_| CustomError(String::from("unable to connect to firebase")))?;
+    //}
 }
 
 fn notification(ran: Arc<AtomicBool>) {
     println!("Yeah!");
 }
 
-async fn notify_at(
-    wakeup_time_epoch: i64,
-    callback: fn(Arc<AtomicBool>),
-    ran: Arc<AtomicBool>,
-) {
+async fn notify_at(wakeup_time_epoch: i64, callback: fn(Arc<AtomicBool>), ran: Arc<AtomicBool>) {
     let sleep_seconds = wakeup_time_epoch - Utc::now().timestamp();
-    task::block_on(async move {
-    task::sleep(Duration::from_secs(sleep_seconds as u64)).await });
+    task::block_on(async move { task::sleep(Duration::from_secs(sleep_seconds as u64)).await });
     callback(ran);
 }
 
@@ -68,12 +66,19 @@ fn done(ran: Arc<AtomicBool>) {
 #[derive(Debug)]
 struct CustomError(String);
 
-fn firebase() -> Result<Firebase,UrlParseError> {
+static FIREBASE_URL: &str = "https://rust-timer-default-rtdb.firebaseio.com";
+static BAD_FIREBASE_URL: &str = "rust-timer-default-rtdb.firebaseio.com";
+
+fn firebase() -> Result<Firebase, UrlParseError> {
     Firebase::new(FIREBASE_URL) //.map_err(|x| CustomError(format!("unable to create Firebase")))?;
 }
 
-fn store_future_time(given_time: Option<i64>, wait_minutes: i64, uid: &str) -> Result<i64, CustomError> {
-//fn store_future_time(given_time: Option<i64>, wait_minutes: i64, uid: &str) -> Result<i64, UrlParseError> {
+fn store_future_time(
+    firebase: &Firebase,
+    given_time: Option<i64>,
+    wait_minutes: i64,
+    uid: &str,
+) -> Result<i64, CustomError> {
     let start_time_epoch = match given_time {
         Some(time) => time,
         None => Utc::now().timestamp(),
@@ -81,39 +86,33 @@ fn store_future_time(given_time: Option<i64>, wait_minutes: i64, uid: &str) -> R
 
     let end_time = start_time_epoch + wait_minutes * 60;
     let end_time_text = format!("{{\"endTime\":{}}}", end_time);
-    //let timer = firebase().at(uid).map_err(|x| std::fmt::Error)?;
-    // let timer = firebase().at(uid).map_err(|x| {println!("==========> {:?}", x); return std::fmt::Error} )?;
-    let firebase = firebase().map_err(|_| {
-        println!("firebase problem");
-        CustomError(format!("unable to connect to firebase"))
-    })?;
-    println!("should not see this");
-    let timer = firebase.at(uid)
-        .map_err(|x| {
-            println!("map err on x");
-            return CustomError(format!("Hey toes"));
-        }) ? ;
-    println!("Hi mum {:?}", timer);
+    let timer = firebase
+        .at(uid)
+        .map_err(|_| CustomError(String::from("Hey toes")))?;
 
-    // timer.set(&end_time_text).map_err(|_| std::fmt::Error)?;
+    timer
+        .set(&end_time_text)
+        .map_err(|_| CustomError(String::from("Unable to set timer")))?;
 
     return Ok(end_time);
 }
 
-// fn retrieve_future_time(uid: &str) -> Result<i64> {
-//     let timer = firebase().at(uid).map_err(|_| std::fmt::Error)?;
-//     let json_payload = timer.get().map_err(|_| std::fmt::Error)?;
-//     let node: Value = serde_json::from_str(&json_payload.body)?;
-//
-//     // TODO: What if endtime is garbage and can't convert to i64?
-//     // let end_time_i64 = node["endTime"].as_i64().unwrap();
-//     let end_time_i64 = node["endTime"].as_i64();
-//     match end_time_i64 {
-//         Some(x) => {println!("====> Go some: {}", x) ; return Ok(x)},
-//         None => {println!("===> Got none"); return Ok(0);},
-//     }
-//     //return Ok(end_time_i64);
-// }
+fn retrieve_future_time(firebase: &Firebase, uid: &str) -> Result<i64, CustomError> {
+    let timer = firebase
+        .at(uid)
+        .map_err(|_| CustomError(String::from("Could not set timer")))?;
+    let json_payload = timer
+        .get()
+        .map_err(|_| CustomError(String::from("Could not get timer payload")))?;
+    let node: Value = serde_json::from_str(&json_payload.body)
+        .map_err(|_| CustomError(String::from("unable parse json")))?;
+
+    // TODO: What if endtime is garbage and can't convert to i64?
+    // let end_time_i64 = node["endTime"].as_i64().unwrap();
+    node["endTime"]
+        .as_i64()
+        .ok_or(CustomError(String::from("Could not convert to i64")))
+}
 
 #[cfg(test)]
 mod tests {
@@ -143,35 +142,22 @@ mod tests {
     // }
 
     #[test]
-    /*
     fn store_future_time_from_duration() {
-        println!("store_future_time_from_duration");
         let wait_minutes: i64 = 5;
         let start_time_epoch = 0;
         let uid = "TEST123456ABC";
+        // THIS CODE: will have to be done by someone else
+        let firebase = firebase().unwrap();
 
-        store_future_time(Some(start_time_epoch), wait_minutes, uid);
+        let result = store_future_time(&firebase, Some(start_time_epoch), wait_minutes, uid);
 
-        let end_time = retrieve_future_time(uid).unwrap();
+        let end_time = retrieve_future_time(&firebase, uid).unwrap();
         assert_eq!(end_time, 300)
     }
-    */
-    fn store_future_time_from_duration() {
-        println!("store_future_time_from_duration");
-        let wait_minutes: i64 = 5;
-        let start_time_epoch = 0;
-        let uid = "TEST123456ABC";
+}
 
-        let result = store_future_time(Some(start_time_epoch), wait_minutes, uid);
-
-        match result {
-            Ok(x) => { println!("====> Got some: {}", x); },
-            Err(e) => { println!("===> Error {:?}", e); },
-        }
-    }
-
-    // #[test]
-    // fn store_future_time_returns_end_time() {
+// #[test]
+// fn store_future_time_returns_end_time() {
     //     println!("store_future_time_returns_end_time");
     //     let wait_minutes: i64 = 5;
     //     let start_time_epoch = 0;
@@ -181,4 +167,3 @@ mod tests {
     //
     //     assert_eq!(end_time_result.unwrap(), 300);
     // }
-}
